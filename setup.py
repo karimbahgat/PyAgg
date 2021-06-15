98dd6b4e85eb60030fe5ca29e689ed057d05b60e
Karim Bahgat <karim.bahgat.norway@gmail.com>
2021-06-15 17:06:30 +0200

    Update to correct requires arg

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(	long_description=open("README.rst").read(),
 	version="""0.3.0""",
 	keywords="""graphics rendering drawing visualization imaging AGG aggdraw""",
 	packages=['pyagg'],
-	requires=['PIL', 'aggdraw'],
+	install_requires=['PIL', 'aggdraw'],
 	classifiers=['License :: OSI Approved', 'Programming Language :: Python', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Intended Audience :: Science/Research', 'Intended Audience :: End Users/Desktop', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Multimedia :: Graphics', 'Topic :: Scientific/Engineering :: GIS'],
 	description="""Simple user-oriented graphics drawing and image manipulation.""",
 	)
